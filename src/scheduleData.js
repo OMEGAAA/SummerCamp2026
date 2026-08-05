2a4177de33a1cb5dd949e65d70bd2502178bdda7
genki0430to <genki0430to@gmail.com>
2026-08-05 12:58:51 +0900

    feat: add scheduleData.js to store static scheduling information

--- src/scheduleData.js
+++ src/scheduleData.js
@@ -46,8 +46,8 @@ export const schedule = {
     jr: { activity: "run", level: 2, seats: 10 },
   },
   7: {
-    am: { activity: "run", level: 1, seats: 2 },
-    pm: { activity: "run", level: 2, seats: 7 },
+    am: { activity: "run", level: 1, seats: 0 },
+    pm: { activity: "run", level: 2, seats: 6 },
     jr: { activity: "run", level: 1, seats: 10 },
   },
   17: {
@@ -66,7 +66,7 @@ export const schedule = {
     jr: { activity: "agility", level: 1, seats: 10 },
   },
   21: {
-    am: { activity: "run", level: 1, seats: 10 },
+    am: { activity: "run", level: 1, seats: 9 },
     pm: { activity: "run", level: 2, seats: 10 },
     jr: { activity: "run", level: 1, seats: 10 },
   },
